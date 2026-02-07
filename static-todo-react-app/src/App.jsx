8f7bb0f856dab67771d908aeaf3dec5e6fa3019d
mchaudhry02 <mchaudhry20@hotmail.com>
2026-02-07 14:58:00 -0600

    done

---
+++ static-todo-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from "./components/Header";
+import ToDoList from "./components/ToDoList";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <><div className="app">
+        <Header />
+        <ToDoList />
+      </div></>
+  )
+}
+
+export default App
